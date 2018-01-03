dc8e44a699329471059a6b4d3926170f04d13458
ara <aravindc@sivapriya.org>
2018-01-03 17:51:18 +0000

    View message

--- myimap.py
+++ myimap.py
@@ -19,9 +19,14 @@ if __name__=='__main__':
     c = open_connection(verbose=True)
     try:
         c.list()
-        c.select('inbox')
+        c.select(mailbox='INBOX',readonly=True)
         result, data = c.uid('search', None, "ALL")
-        i = len(data[0].split())
-        print('Response: ', i)
+        for x in data[0].split():
+            ret, data = c.fetch(x,'(BODY.PEEK[HEADER])')
+            if ret != 'OK':
+                print('Error with message: ',x)
+                return
+            print(data)
+            break
     finally:
         c.logout()
