b5222a6383651c66e6ef12a71fbd23da1ef9a718
ara <aravindc@sivapriya.org>
2018-01-03 16:53:13 +0000

    Fixed some code

--- myimap.py
+++ myimap.py
@@ -19,13 +19,7 @@ if __name__=='__main__':
     c = open_connection(verbose=True)
     try:
         typ, data = c.list()
-<<<<<<< HEAD
         print('Response Code: ', typ)
         print('Response: ', data)
-=======
-        print('Response Code: ',typ)
-        print('Response: ')
-        pprint(data)
->>>>>>> 6aeeac524f0b7f62ab55e5b9549b6d54007692c2
     finally:
         c.logout()
