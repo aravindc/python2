139b536f6763aa694bf8b355e0d8ee67eb1eb82f
ara <aravindc@sivapriya.org>
2018-01-03 23:07:13 +0000

    Working version

--- myimap.py
+++ myimap.py
@@ -1,10 +1,14 @@
 import imaplib
-import ConfigParser
+import configparser
 import os
 import email
+import datetime
+import codecs
+
+now = datetime.datetime.now()
 
 def open_connection(verbose=False):
-    config = ConfigParser.ConfigParser()
+    config = configparser.ConfigParser()
     config.read([os.path.expanduser('cred.cfg')])
 
     hostname = config.get('server', 'hostname')
@@ -17,6 +21,7 @@ def open_connection(verbose=False):
     connection.login(username, password)
     return connection
 if __name__=='__main__':
+    f = codecs.open(now.strftime("%Y%m%d") + '.csv', 'a', 'utf-8')
     c = open_connection(verbose=True)
     try:
         c.list()
@@ -26,10 +31,17 @@ if __name__=='__main__':
             ret, data = c.fetch(x,'(RFC822)')
             if ret != 'OK':
                 print('Error with message: ',x)
-            msg = email.message_from_string(data[0][1])
-            hdr = email.header.make_header(email.header.decode_header(msg['Subject']))
-            subject = str(hdr)
-            print('Message %s: %s' % (x, subject))
-            break
+            msg = email.message_from_bytes(data[0][1])
+            fromhdr = email.header.make_header(email.header.decode_header(msg['From']))
+            fromstr = str(fromhdr).replace('\n', ' ')
+            subjhdr = email.header.make_header(email.header.decode_header(msg['Subject']))
+            subjstr = str(subjhdr).replace('\n', ' ')
+            date_tuple = email.utils.parsedate_tz(msg['Date'])
+            if date_tuple:
+                local_date = datetime.datetime.fromtimestamp(email.utils.mktime_tz(date_tuple))
+            # print ("Local Date:", local_date.strftime("%a, %d %b %Y %H:%M:%S"))
+            # print('%s|%s|%s' % (local_date.strftime("%Y-%m-%d %H:%M:%S"),fromstr, subjstr))
+            f.write('%s|%s|%s\n' % (local_date.strftime("%Y-%m-%d %H:%M:%S"),fromstr, subjstr))
     finally:
         c.logout()
+        f.close()
