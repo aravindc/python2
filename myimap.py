a311e8095898c6fc55970578a01be2529720f618
ara <aravindc@sivapriya.org>
2018-01-04 10:22:42 +0000

    include uid to output

--- myimap.py
+++ myimap.py
@@ -26,22 +26,25 @@ if __name__=='__main__':
     try:
         c.list()
         c.select(mailbox='INBOX',readonly=True)
-        result, emdata = c.uid('search', None, "ALL")
+        #result, emdata = c.uid('search', None, "ALL")
+        result, emdata = c.sort('DATE', 'UTF-8', 'ALL')
         for x in emdata[0].split():
+            print('Processing: ',x)
             ret, data = c.fetch(x,'(RFC822)')
             if ret != 'OK':
                 print('Error with message: ',x)
             msg = email.message_from_bytes(data[0][1])
+            #print(str(msg))
             fromhdr = email.header.make_header(email.header.decode_header(msg['From']))
-            fromstr = str(fromhdr).replace('\n', ' ')
+            fromstr = str(fromhdr).replace('\n', ' ').replace('\r', ' ')
             subjhdr = email.header.make_header(email.header.decode_header(msg['Subject']))
-            subjstr = str(subjhdr).replace('\n', ' ')
+            subjstr = str(subjhdr).replace('\n', ' ').replace('\r', ' ')
             date_tuple = email.utils.parsedate_tz(msg['Date'])
             if date_tuple:
                 local_date = datetime.datetime.fromtimestamp(email.utils.mktime_tz(date_tuple))
             # print ("Local Date:", local_date.strftime("%a, %d %b %Y %H:%M:%S"))
             # print('%s|%s|%s' % (local_date.strftime("%Y-%m-%d %H:%M:%S"),fromstr, subjstr))
-            f.write('%s|%s|%s\n' % (local_date.strftime("%Y-%m-%d %H:%M:%S"),fromstr, subjstr))
+            f.write('%s|%s|%s|%s\n' % x, (local_date.strftime("%Y-%m-%d %H:%M:%S"),fromstr, subjstr))
     finally:
         c.logout()
         f.close()
